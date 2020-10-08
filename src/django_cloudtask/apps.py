fe98e7481d2ef87a65c1a325bbab720a7f960f8f
Alec McGavin <alec.mcgavin@kogan.com.au>
2020-10-08 16:44:16 +1100

    Fix app resolution, task discovery

--- src/django_cloudtask/apps.py
+++ src/django_cloudtask/apps.py
@@ -1,24 +1,15 @@
-import os
 from importlib import import_module
 
 from django.apps import AppConfig
 
 
 class TasksConfig(AppConfig):
-    name = "tasks"
+    name = "django_cloudtask"
 
     def ready(self):
-        # go through all apps and register tasks.
-        dirname = os.path.dirname(self.path)
         for cfg in self.apps.get_app_configs():
-            path = cfg.path
-            if os.path.dirname(path) != dirname:
-                continue
-
-            module = os.path.relpath(cfg.path, dirname).replace("/", ".")
             try:
-                import_module(f"{module}.tasks")
+                import_module(f"{cfg.name}.tasks")
             except ImportError as e:
-                if f"{module}.tasks" not in str(e):
+                if f"{cfg.name}.tasks" not in str(e):
                     raise
-                continue
